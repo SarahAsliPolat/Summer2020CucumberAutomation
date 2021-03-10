0b2ab5cb756e283780eccd252b368f8897b818b2
SarahAsliPolat <asliatamanpolat@gmail.com>
2021-03-10 01:15:57 -0500

    new version

--- src/test/java/com/vytrack/pages/CreateCarPage.java
+++ src/test/java/com/vytrack/pages/CreateCarPage.java
@@ -1,5 +1,6 @@
 package com.vytrack.pages;
 
+import com.vytrack.utils.BrowserUtils;
 import com.vytrack.utils.Driver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -12,10 +13,24 @@ public class CreateCarPage extends BasePage {
     @FindBy(css = "[title='Create Car']")
     private WebElement createCarBtn;
 
+    @FindBy(name = "custom_entity_type[LicensePlate]")
+    private WebElement licencePlateInputBox;
+
+    @FindBy(name = "custom_entity_type[ModelYear]")
+    private WebElement modelYearInputBox;
+
     public void clickOnCreateCar(){
         WebDriverWait wait =new WebDriverWait(Driver.getDriver(),20);
         wait.until(ExpectedConditions.elementToBeClickable(createCarBtn)).click();
         System.out.println("Clicking on 'Create Car' button");
+
+    }
+    public void setLicencePlateInputBox(String licencePlate){
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(),20);
+        wait.until(ExpectedConditions.elementToBeClickable(licencePlateInputBox));
+
+        licencePlateInputBox.clear();
+        licencePlateInputBox.sendKeys(licencePlate);
 
     }
 
--- src/test/java/com/vytrack/step_definitions/Car_Step_Definitions.java
+++ src/test/java/com/vytrack/step_definitions/Car_Step_Definitions.java
@@ -1,18 +1,25 @@
 package com.vytrack.step_definitions;
 
+import com.vytrack.pages.CreateCarPage;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 
+import java.util.Map;
+
 public class Car_Step_Definitions {
+    CreateCarPage createCarPage =new CreateCarPage();
 
     @Given("user clicks on create car button")
     public void user_clicks_on_create_car_button() {
-
+        createCarPage.clickOnCreateCar();
 
     }
 
     @When("user adds new vehicle information")
-    public void user_adds_new_vehicle_information(io.cucumber.datatable.DataTable dataTable) {
+    public void user_adds_new_vehicle_information(Map<String, String> dataTable) {//in order to be able insert the data we changed the parameter to Map.We chose String for both because we will use send.Keys method.
+
+
+
 
     }
 
